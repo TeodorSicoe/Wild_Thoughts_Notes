fc81c8c76e281641b1a3b70ade60b2068d4eb9c5
TeodorSicoe <teodorsicoe@gmail.com>
2022-01-25 23:45:57 +0200

    new note at every click

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const handleClick = () => {
+    let newNote = document.createElement('input');
+    newNote.classList.add('note');
+    document.getElementById('grid-container').appendChild(newNote);
+
+
+
+}
